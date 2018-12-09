274a4914d2c0a8965a0a98d7aa8a7e6e647cdce5
David Thorpe <djt@mutablelogic.com>
2018-12-09 15:52:55 +0000

    Updated pwm example

--- cmd/pwm/pwm.go
+++ cmd/pwm/pwm.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"time"
 
 	// Frameworks
 	"github.com/djthorpe/gopi"
@@ -12,12 +12,57 @@ import (
 	_ "github.com/djthorpe/gopi/sys/logger"
 )
 
+////////////////////////////////////////////////////////////////////////////////
+
+type Motor struct {
+	plus, minus gopi.GPIOPin
+	factor      float32
+	pwm         gopi.PWM
+}
+
+func NewMotor(pwm gopi.PWM, plus, minus gopi.GPIOPin, invert bool) *Motor {
+	this := new(Motor)
+	this.plus = plus
+	this.minus = minus
+	this.pwm = pwm
+	if invert {
+		this.factor = -1.0
+	} else {
+		this.factor = 1.0
+	}
+	return this
+}
+
+func (this *Motor) SetSpeed(speed float32) {
+	speed = speed * this.factor
+	if speed < -1.0 {
+		speed = -1.0
+	}
+	if speed > 1.0 {
+		speed = 1.0
+	}
+	if speed < 0.0 {
+		this.pwm.SetDutyCycle(-speed, this.minus)
+		this.pwm.SetDutyCycle(0, this.plus)
+	} else if speed > 0.0 {
+		this.pwm.SetDutyCycle(speed, this.plus)
+		this.pwm.SetDutyCycle(0, this.minus)
+	} else {
+		this.pwm.SetDutyCycle(0.0, this.plus, this.minus)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 const (
 	// Motor, via DRV8833PWP Dual H-Bridge
-	M1B = gopi.GPIOPin(19)
-	M1F = gopi.GPIOPin(20)
-	M2B = gopi.GPIOPin(21)
-	M2F = gopi.GPIOPin(26)
+	// In order to use this with pi-blaster, you will also need to
+	// add the line to the file /etc/default/pi-blaster
+	// DAEMON_OPTS=-g 19,20,21,26
+	M1P = gopi.GPIOPin(19)
+	M1N = gopi.GPIOPin(20)
+	M2P = gopi.GPIOPin(21)
+	M2N = gopi.GPIOPin(26)
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,12 +73,17 @@ func mainLoop(app *gopi.AppInstance, done chan<- struct{}) error {
 		return app.Logger.Error("Missing PWM module instance")
 	}
 
-	// Stop motors
-	if err := app.PWM.SetDutyCycle(0, M1B, M1F, M2B, M2F); err != nil {
-		return err
-	}
+	// Set up motors
+	right := NewMotor(app.PWM, M1P, M1N, false)
+	left := NewMotor(app.PWM, M2P, M2N, true)
 
-	fmt.Println(app.PWM)
+	left.SetSpeed(1.0)
+	right.SetSpeed(-1.0)
+
+	time.Sleep(1.0 * time.Second)
+
+	left.SetSpeed(0.0)
+	right.SetSpeed(0.0)
 
 	// Finished
 	done <- gopi.DONE
--- go.mod
+++ go.mod
@@ -2,5 +2,5 @@ module github.com/djthorpe/robots
 
 require (
 	github.com/djthorpe/gopi v1.0.30
-	github.com/djthorpe/gopi-hw v1.0.7
+	github.com/djthorpe/gopi-hw v1.0.8
 )
